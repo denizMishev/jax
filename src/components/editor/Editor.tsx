1735797af1b09eca3c35e2371d045198e305550c
Mishev Deniz <denizmishev@yahoo.com>
2025-02-06 22:23:42 +0200

    added marked for text to markdown conversion, added tailwind typography for markdown styling, removed tailwind config file as they're deprecated in tailwind v4

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
+import { marked } from "marked";
+
 import { TextPanel } from "./TextPanel";
 import { PreviewPanel } from "./PreviewPanel";
 
 export function Editor() {
   const [text, setText] = useState("");
+  const [htmlContent, setHtmlContent] = useState("");
 
   const handleTextChange = (newText: string) => {
     setText(newText);
-  };
 
-  console.log(text, "text");
+    const html = marked(newText);
+    setHtmlContent(html);
+  };
 
   return (
     <main className="w-full h-[calc(100vh-90px)] py-6">
       <div className="h-full w-full flex justify-around">
         <TextPanel onTextChange={handleTextChange}></TextPanel>
-        <PreviewPanel></PreviewPanel>
+        <PreviewPanel content={htmlContent}></PreviewPanel>
       </div>
     </main>
   );
--- src/components/editor/PreviewPanel.tsx
+++ src/components/editor/PreviewPanel.tsx
@@ -1,3 +1,14 @@
-export function PreviewPanel() {
-  return <div className="bg-[#dcdcdc33] rounded-lg shadow-md w-[47%]"></div>;
+interface PreviewPanelProps {
+  content: string;
+}
+
+export function PreviewPanel({ content }: PreviewPanelProps) {
+  return (
+    <div className="bg-[#dcdcdc33] rounded-lg shadow-md w-[47%]">
+      <div
+        className="prose max-w-none p-4"
+        dangerouslySetInnerHTML={{ __html: content }}
+      ></div>
+    </div>
+  );
 }
